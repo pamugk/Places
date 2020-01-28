99dee085646f6a625052351329f04ef48bf8e721
Alexander <pamugk@gmail.com>
2020-01-28 16:42:38 +0500

    Almost finished the whole app

--- app/src/main/java/ru/psu/places/MapsActivity.kt
+++ app/src/main/java/ru/psu/places/MapsActivity.kt
@@ -1,5 +1,6 @@
 package ru.psu.places
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -10,33 +11,50 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
-    GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
-    private lateinit var positions: MutableList<LatLng>
+    private lateinit var photos: HashMap<LatLng, String?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        positions = mutableListOf()
+        val savedPhotos = savedInstanceState?.getSerializable("markers")
+        photos = if (savedPhotos != null)
+            savedPhotos as HashMap<LatLng, String?>
+        else
+            hashMapOf()
+        val extra = intent.extras
+        if (extra != null && extra.getBoolean("changed"))
+            photos[extra.getParcelable("position")!!] = extra.getString("photoUri")
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("markers", photos)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        map.setOnMapClickListener(this)
-        map.setOnMarkerClickListener(this)
+        map.setOnMapClickListener {coordinates -> onMapClick(coordinates)}
+        map.setOnMarkerClickListener {marker -> onMarkerClick(marker)}
+        photos.keys.forEach { position -> map.addMarker(MarkerOptions().position(position)) }
     }
 
     private fun addMarker(coordinates: LatLng){
-        positions.add(coordinates)
+        photos[coordinates] = null
         map.addMarker(MarkerOptions().position(coordinates))
     }
 
-    override fun onMapClick(coordinates: LatLng) {
+    private fun removeMarker(marker: Marker?){
+        photos.remove(marker!!.position)
+        marker.remove()
+    }
+
+    private fun onMapClick(coordinates: LatLng) {
         AlertDialog.Builder(this)
             .setTitle(R.string.new_marker_dialog_title)
             .setMessage(R.string.new_marker_dialog_message)
@@ -45,12 +63,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
             .show()
     }
 
-    override fun onMarkerClick(clickedMarker: Marker?): Boolean {
+    private fun callPhotoActivity(marker: Marker?) {
+        startActivity(
+            Intent(this, PhotoActivity::class.java)
+                .putExtra("position", marker?.position)
+                .putExtra("photoUri", photos[marker?.position])
+        )
+    }
+
+    private fun onMarkerClick(clickedMarker: Marker?): Boolean {
         AlertDialog.Builder(this)
             .setTitle(R.string.marker_photo_title)
             .setMessage(R.string.marker_photo_message)
-            .setPositiveButton(android.R.string.yes) { _, _ -> }
-            .setNeutralButton(R.string.remove_marker) {_,_ -> clickedMarker?.remove()}
+            .setPositiveButton(android.R.string.yes) { _, _ -> callPhotoActivity(clickedMarker)}
+            .setNeutralButton(R.string.remove_marker) {_,_ -> removeMarker(clickedMarker)}
             .setNegativeButton(android.R.string.no) { _, _->}
             .show()
         return true
--- app/src/main/java/ru/psu/places/PhotoActivity.kt
+++ app/src/main/java/ru/psu/places/PhotoActivity.kt
@@ -1,4 +1,111 @@
 package ru.psu.places
 
-class PhotoActivity {
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_photo.*
+import java.io.File
+import java.io.IOException
+import java.text.DateFormat.getDateTimeInstance
+
+class PhotoActivity: AppCompatActivity() {
+    private lateinit var position: LatLng
+    private var photoUri: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+        newImageBtn.setOnClickListener {makeNewPhoto()}
+        saveBtn.setOnClickListener {saveChanges()}
+        cancelBtn.setOnClickListener {cancelChanges()}
+        val extraThingies = intent.extras
+        if (savedInstanceState != null){
+            position = savedInstanceState.getParcelable("position")!!
+            photoUri = savedInstanceState.getString("photoUri")
+        }
+        if (extraThingies != null) {
+            if (extraThingies.getParcelable<LatLng>("position") != null)
+                position = extraThingies.getParcelable("position")!!
+            photoUri = extraThingies.getString("photoUri")
+        }
+        if (photoUri != null)
+            setPhoto()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable("position", position)
+        outState.putString("photoUri", photoUri)
+    }
+
+    private fun makeNewPhoto(){
+        val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (camera.resolveActivity(packageManager) != null){
+            val photoFile: File?
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
+                println("IOException")
+                return
+            }
+            camera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
+            startActivityForResult(camera, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            try {
+                setPhoto()
+                saveBtn.isEnabled = true
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun setPhoto(){
+        val mImageBitmap = MediaStore.Images.Media.getBitmap(
+            this.contentResolver,
+            Uri.parse(photoUri)
+        )
+        markerPhoto.setImageBitmap(mImageBitmap)
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp = getDateTimeInstance()
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = Environment.getExternalStoragePublicDirectory(
+            DIRECTORY_PICTURES
+        )
+        val image = File.createTempFile(imageFileName,".jpg",storageDir)
+        photoUri = "file:" + image.absolutePath
+        return image
+    }
+
+    private fun saveChanges() {
+        startActivity(
+            Intent(this, MapsActivity::class.java)
+                .putExtra("changed", true)
+                .putExtra("position", position)
+                .putExtra("photoUri", photoUri)
+        )
+    }
+
+    private fun cancelChanges() {
+        startActivity(
+            Intent(this, MapsActivity::class.java)
+                .putExtra("changed", false)
+        )
+    }
 }
